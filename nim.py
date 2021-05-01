69bd03c2559383c1ad047123eb794c7cbc3f6af5
ludovicmantovani <ludovic.mantov@gmail.com>
2021-05-01 20:53:56 +0200

    [ADD]: Display match board

--- nim.py
+++ nim.py
@@ -21,8 +21,11 @@ class Match(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.transform.smoothscale(
-            pygame.image.load("img/match640x640.png"), (64, 64))
-        self.surf = pygame.Surface((64, 64))
+            pygame.transform.rotate(
+                pygame.image.load("img/match640x640.png"),
+                45.0),
+            (90, 90))
+        self.surf = pygame.Surface((90, 90))
 
     def draw(self, surface, position):
         surface.blit(
@@ -30,6 +33,31 @@ class Match(pygame.sprite.Sprite):
             self.surf.get_rect(
                 center=position)
         )
+
+
+def get_coord(nbr_element, elem_size, origin_pos):
+    ret = {}
+    middle = nbr_element / 2
+    for index in range(nbr_element):
+        index_delta = abs(middle - index)
+        pos_delta = elem_size * index_delta
+        if index < middle:
+            pos_delta = pos_delta * -1
+        else:
+            pos_delta = pos_delta * 1
+        glob_pos = pos_delta + + (elem_size / 2) + origin_pos
+        ret[index] = glob_pos
+    return ret
+
+
+def draw_match_board(match_board, screen):
+    lines_nbr = len(match_board)
+    height_val = get_coord(lines_nbr, 100, HEIGHT / 2)
+    for index, row in enumerate(match_board):
+        width_val = get_coord(row, 22.5, WIDTH / 2)
+        for k, val in width_val.items():
+            m = Match()
+            m.draw(screen, (val, height_val[index]))
 
 
 def main():
@@ -45,15 +73,20 @@ def main():
     screen = pygame.display.set_mode(RESOLUTION, pygame.NOFRAME | pygame.SCALED)
     pygame.Surface.fill(screen, DEFAULT_SCREEN_COLOR)
 
-    # Draw elements
-    pygame.draw.line(screen, RED, (0, HEIGHT / 2), (WIDTH, HEIGHT / 2))
-    pygame.draw.line(screen, RED, (WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
-    m1 = Match()
-    m1.draw(screen, (WIDTH / 2, HEIGHT / 2))
-    pygame.display.update()
-
     # define a variable to control the main loop
     running = True
+
+    match_board = [
+        1,
+        3,
+        7,
+        15,
+        15 + 15 + 1
+    ]
+
+    draw_match_board(match_board, screen)
+
+    pygame.display.update()
 
     # main loop
     while running:
@@ -63,11 +96,19 @@ def main():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-
-            keys = pygame.key.get_pressed()
-
-            if keys[pygame.K_ESCAPE] or keys[pygame.key.key_code("escape")]:
-                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE or event.key == pygame.key.key_code("escape"):
+                    running = False
+                elif event.key == pygame.K_SPACE or event.key == pygame.key.key_code("space"):
+                    truncated = False
+                    for index, nb in enumerate(match_board):
+                        if not truncated:
+                            if nb > 0:
+                                match_board[index] = nb - 1
+                                truncated = True
+                    pygame.Surface.fill(screen, DEFAULT_SCREEN_COLOR)
+                    draw_match_board(match_board, screen)
+                    pygame.display.update()
 
 
 # run the main function only if this module is executed as the main script
